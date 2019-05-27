f77ab98cb6ab63a09459e9fe6454c9d0dfb149ec
Alan Laredo <AlanLaredo@hotmail.com>
2019-05-26 20:47:36 -0500

    Updates

---
+++ precache-manifest.6220e218029b0c2ff81912598aad8a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4e244a2ca5cd65646f909f3dc9200b",
+    "url": "/karte/index.html"
+  },
+  {
+    "revision": "80353f0991769812d9a5",
+    "url": "/karte/static/css/main.d34c3406.chunk.css"
+  },
+  {
+    "revision": "a288ab24967c04d3bcba",
+    "url": "/karte/static/js/2.ffc71a18.chunk.js"
+  },
+  {
+    "revision": "80353f0991769812d9a5",
+    "url": "/karte/static/js/main.6225060e.chunk.js"
+  },
+  {
+    "revision": "1837386fbc0c4c9167ff",
+    "url": "/karte/static/js/runtime~main.486a739a.js"
+  },
+  {
+    "revision": "e5dead1e092368430db07494517cef55",
+    "url": "/karte/static/media/fondo.e5dead1e.png"
+  }
+]);
